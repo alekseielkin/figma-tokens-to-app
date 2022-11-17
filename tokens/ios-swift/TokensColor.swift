f40788178ffd70ae063671041088561dc440042a
alekseielkin <aleksei@rankia.com>
2022-11-17 17:06:09 +0100

    after npm run tokens

---
+++ tokens/ios-swift/TokensColor.swift
@@ -0,0 +1,24 @@
+
+//
+// TokensColor.swift
+//
+// Do not edit directly
+// Generated on Thu, 17 Nov 2022 13:00:56 GMT
+//
+
+import UIKit
+
+public enum TokensColor {
+    public static let alertError = UIColor(red: 0.898, green: 0.212, blue: 0.227, alpha:1)
+    public static let alertSuccess = UIColor(red: 0.204, green: 0.918, blue: 0.486, alpha:1)
+    public static let alertWarning = UIColor(red: 1.000, green: 0.663, blue: 0.051, alpha:1)
+    public static let neutral00 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha:1)
+    public static let neutral05 = UIColor(red: 0.949, green: 0.949, blue: 0.949, alpha:1)
+    public static let neutral20 = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha:1)
+    public static let neutral40 = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha:1)
+    public static let neutral80 = UIColor(red: 0.200, green: 0.200, blue: 0.200, alpha:1)
+    public static let primaryAccent = UIColor(red: 0.149, green: 0.945, blue: 0.565, alpha:1)
+    public static let primaryDark = UIColor(red: 0.114, green: 0.412, blue: 0.561, alpha:1)
+    public static let primaryLight = UIColor(red: 0.337, green: 0.643, blue: 0.788, alpha:1)
+    public static let primaryMain = UIColor(red: 0.965, green: 0.031, blue: 0.200, alpha:1)
+}
